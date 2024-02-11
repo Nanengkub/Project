d15e91a94dc0a691b01e64cefb9ab28e29eae3b0
napapha_nan <148744310+Nanengkub@users.noreply.github.com>
2024-02-11 12:07:40 +0700

    v3

---
+++ views/Q1View.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="Q1">
+      <p style="font-size:60px;">ทับสองคนไหนอยู่ 07 ไลน์?</p>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 10.5cm; right: 21.5cm;" @click="wrong()"> A
+        <img src="../assets/toey.jpg" width="200" height="215">
+        <p style="font-size:20px;">ก้องภพ</p>
+      </button>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 10.5cm; right: 12cm;" @click="wrong()"> B
+        <img src="../assets/pete.jpg" width="200" height="215">
+        <p style="font-size:20px;">เอกชลิต</p>
+      </button>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 2cm; right: 21.5cm;" @click="correct()"> C
+        <img src="../assets/fah.jpg" width="200" height="215">
+        <p style="font-size:20px;">นภัส</p>
+      </button>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 2cm; right: 12cm;" @click="wrong()"> D
+        <img src="../assets/pan.jpg" width="200" height="215">
+        <p style="font-size:20px;">ณัฐนิชา</p>
+      </button>
+    </div>
+  </template>
+  <script>
+  export default {
+
+    name: 'App',
+    methods:{
+      correct(){
+      localStorage.setItem("auth",false);
+      this.$router.push({name:'corr1'});
+      },
+      wrong(){
+      localStorage.setItem("auth",false);
+      this.$router.push({name:'wrong1'});
+      },
+    }
+    }
+  </script>
+
+<style>
+  body {
+    background-color: #ddfec2;
+  }
+</style>
---
+++ views/Q3View.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="Q3">
+      <p style="font-size:60px;">ใครแพร่เชื้อวัณโรค ครั้งล่าสุดของทับสอง?</p>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 10.5cm; right: 21.5cm;" @click="wrong()"> A
+        <img src="../assets/touch.jpg" width="200" height="215">
+        <p style="font-size:20px;">นันทชัย</p>
+      </button>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 10.5cm; right: 12cm;" @click="wrong()"> B
+        <img src="../assets/nan.jpg" width="200" height="215">
+        <p style="font-size:20px;">ณปภา</p>
+      </button>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 2cm; right: 21.5cm;" @click="wrong()"> C
+        <img src="../assets/fah.jpg" width="200" height="215">
+        <p style="font-size:20px;">นภัส</p>
+      </button>
+      <button type="button" style="width:250px; height:250px; font-size:25px; position: fixed; bottom: 2cm; right: 12cm;" @click="correct()"> D
+        <img src="../assets/toey.jpg" width="200" height="215">
+        <p style="font-size:20px;">ก้องภพ</p>
+      </button>
+    </div>
+  </template>
+  <script>
+  export default {
+
+    name: 'App',
+    methods:{
+      correct(){
+      localStorage.setItem("auth",false);
+      this.$router.push({name:'corr3'});
+      },
+      wrong(){
+      localStorage.setItem("auth",false);
+      this.$router.push({name:'wrong3'});
+      },
+    }
+    }
+  </script>
+
+<style>
+  body {
+    background-color: #ddfec2;
+  }
+</style>
